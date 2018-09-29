13e77f58599fb9bc9076b0bcecbc5f44e1804fd1
Andrea <andrea@MacBook-Air-5.local>
2018-09-29 14:50:59 -0400

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,12 @@
+public class HelloNumber {
+	public static void main(String[] args) {
+	int x = 0;
+	int y = 0;
+	while (x<10) {
+		y = y + x;
+		System.out.print(y + " ");
+		x = x + 1;
+		}
+	}
+}
+
