a4574a4441e8a1ab9ae05a8c2df8bf569d2752a8
Andrea <andrea@MacBook-Air-5.local>
2018-09-29 19:49:34 -0400

    fix bug of LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,12 +8,14 @@ public class LeapYear {
      */
 
     public static boolean isLeapYear(int year) {
-        if year % 400 = 0 {
+        if (year % 400 == 0) {
             return true;
         }
-        else if （year % 4 = 0 and year % 100 != 0) {
+        
+        else if (year % 4 == 0 && year % 100 != 0) {
             return true;
-} 
+        }
+        
         else {
             return false;
         }
@@ -43,6 +45,7 @@ public class LeapYear {
         }
     }
 }
+
 
 
 
